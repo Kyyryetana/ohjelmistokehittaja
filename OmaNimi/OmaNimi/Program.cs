08902cc6643019153a3d57a4d99c4deb6d02c613
nikoalander1986 <niko.alander@hotmail.com>
2022-02-15 09:50:09 +0000

    C# testi

---
+++ OmaNimi/OmaNimi/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OmaNimi
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Niko Alander");
+            Console.ReadLine();
+        }
+    }
+}
