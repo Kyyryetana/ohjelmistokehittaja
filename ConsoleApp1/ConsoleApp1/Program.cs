3719deb3491c11911b4ddc03d43edfad387cb303
nikoalander1986 <niko.alander@hotmail.com>
2022-03-02 18:53:04 +0000

    harjoittelua

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int arvottu;
+            Random luku = new Random();
+
+            for (int i = 1; i <= 7; i++)
+            {
+                arvottu = luku.Next(1, 39);
+                Console.Write("{0}, ", arvottu);
+            }
+            Console.ReadLine();
+        }
+    }
+}
